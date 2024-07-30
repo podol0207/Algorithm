494a2474293bd4f2be7e257cbd30c3c86f6c4424
podol0207 <31719698+podol0207@users.noreply.github.com>
2024-07-30 15:53:35 +0900

    [Silver V] Title: 나이순 정렬, Time: 884 ms, Memory: 53616 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+        });
+
+        for(int i=0; i<N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
