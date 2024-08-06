9420ab8797948becb37b886736fb8c40e6c5c336
podol0207 <31719698+podol0207@users.noreply.github.com>
2024-08-06 13:50:16 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1296 ms, Memory: 98392 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean[] check;
+    static int count = 0;
+    static int[] node;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        node = new int[n+1];
+        for(int i=1; i<=n; i++) {
+            node[i] = 0;
+        }
+        A = new ArrayList[n+1];
+        for(int i=1; i<=n; i++) {
+            A[i] = new ArrayList<Integer>();
+        }
+        check = new boolean[n+1];
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+        for(int i=1; i<=n; i++) {
+            Collections.sort(A[i]);
+        }
+        DFS(r);
+        for(int i=1; i<=n; i++) {
+            if(node[i] == 0) bw.write(0 + "\n");
+            else bw.write(node[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void DFS(int r) {
+        check[r] = true;
+        count++;
+        node[r] = count;
+
+        for(int i=0; i<A[r].size(); i++) {
+            int next = A[r].get(i);
+            if(!check[next]) {
+                check[next] = true;
+                DFS(next);
+            }
+        }
+    }
+}
