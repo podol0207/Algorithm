b5adc86232c496f206f810983e1e8bc4d9965953
podol0207 <31719698+podol0207@users.noreply.github.com>
2024-08-10 22:46:15 +0900

    [Gold IV] Title: 공유기 설치, Time: 324 ms, Memory: 30424 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        int low = 1;
+        int high = arr[n-1];
+
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            int position = 0;
+            int count = 1;
+            for(int i=1; i<n; i++) {
+                if(arr[i]-arr[position] >= mid) {
+                    position =i;
+                    count++;
+                }
+            }
+            if(count < c) {
+                high = mid - 1;
+                continue;
+            }
+            low = mid + 1;
+        }
+        bw.write(low-1 + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
