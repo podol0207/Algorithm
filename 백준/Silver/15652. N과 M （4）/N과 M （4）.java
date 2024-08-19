76411e69fada9fd33545b22b161867046bcf14d9
podol0207 <31719698+podol0207@users.noreply.github.com>
2024-08-19 15:10:57 +0900

    [Silver III] Title: N과 M (4), Time: 140 ms, Memory: 16876 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] A;
+    static int n, m;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        A = new int[m];
+
+        go(1, 0);
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void go(int start, int index) throws IOException {
+        if (index == m) {
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<m; i++) {
+                sb.append(A[i]).append(" ");
+            }
+            sb.append('\n');
+            bw.write(sb.toString());
+            return;
+        }
+        for(int i=start; i<=n; i++) {
+            A[index] = i;
+            go(i, index+1);
+        }
+    }
+}
