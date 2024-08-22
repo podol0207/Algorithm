e1c0589b0afb2f1a6d05a98403490447f4ae2435
podol0207 <31719698+podol0207@users.noreply.github.com>
2024-08-22 17:08:41 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for(int i=0; i<n; i++) {
+            int rank = 1;
+            for(int j=0; j<n; j++) {
+                if(i==j) continue;
+
+                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            bw.write(rank + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
