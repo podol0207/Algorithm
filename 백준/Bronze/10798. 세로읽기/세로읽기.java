33681f03b657e6f244ff877e27c121cb1d6e95cf
podol0207 <31719698+podol0207@users.noreply.github.com>
2025-01-16 15:28:32 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,29 +1,27 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        char[][] str = new char[5][15];
-        int max = 0;
-        
+
+        Character[][] arr = new Character[5][15];
         for(int i=0; i<5; i++) {
-            String word = br.readLine();
-            if(word.length()>max) max = word.length();
-            
-            for(int j=0; j<word.length(); j++) {
-                str[i][j] = word.charAt(j);
+            String line = br.readLine();
+            for(int j=0; j<line.length(); j++) {
+                arr[i][j] = line.charAt(j);
             }
         }
-        
-        for(int i=0; i<max; i++) {
+        for(int i=0; i<15; i++) {
             for(int j=0; j<5; j++) {
-                if(i<str[j].length && str[j][i] != '\0') {
-                   bw.write(str[j][i]);
+                if(arr[j][i]!=null) {
+                    System.out.print(arr[j][i]);
                 }
             }
         }
+
+        bw.flush();
         bw.close();
     }
 }
